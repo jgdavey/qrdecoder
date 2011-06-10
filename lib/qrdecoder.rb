17b0cd68b8af410bb61ff301f27d5172f5d512a0
Dave Lyon and Joshua Davey <dev+davelyon+jgdavey@hashrocket.com>
2011-06-10 09:39:33 -0500

    Make helper method private

--- lib/qrdecoder.rb
+++ lib/qrdecoder.rb
@@ -8,13 +8,14 @@ module QRDecoder
     decode_from_path(absolute_path(path))
   end
 
-  def self.absolute_path(path)
-    path.respond_to?(:path) ? path.path : File.expand_path(path.to_s)
-  end
-
   def self.decode_url(path)
     URI.parse(decode(path))
   rescue URI::InvalidURIError
     nil
+  end
+
+  private
+  def self.absolute_path(path)
+    path.respond_to?(:path) ? path.path : File.expand_path(path.to_s)
   end
 end
