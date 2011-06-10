2a0d4f8deff59fce7bf4f81c99446cc1ffc15db0
Joshua Davey <josh@joshuadavey.com>
2011-06-09 21:35:27 -0500

    Add 1.8.7 compatible path cleaning

--- lib/qrdecoder.rb
+++ lib/qrdecoder.rb
@@ -3,6 +3,10 @@ require 'qrdecoder/qrdecoder_ext'
 
 module QRDecoder
   def self.decode(path)
-    decode_from_path(File.realpath(path))
+    decode_from_path(absolute_path(path))
+  end
+
+  def self.absolute_path(path)
+    path.respond_to?(:path) ? path.path : File.expand_path(path.to_s)
   end
 end
